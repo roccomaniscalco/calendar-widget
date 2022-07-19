ee7aabc38a672a260fd508a66641196979338fd5
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-07-19 06:04:51 -0400

    implement mantine theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { AppShell, Center, Container, Group, Header, Text } from "@mantine/core"
+import DarkModeToggle from "~/DarkModeToggle"
+
+const appHeader = (
+  <Header height={70}>
+    <Container size="sm" sx={{ height: "100%" }}>
+      <Group position="apart" sx={{ height: "100%" }}>
+        <Text size="xl" weight="bold">
+          Calendar Widget
+        </Text>
+        <DarkModeToggle />
+      </Group>
+    </Container>
+  </Header>
+)
+
 const App = () => {
   return (
-    <div>
-      <h1>hello world!</h1>
-    </div>
+    <AppShell padding={0} header={appHeader}>
+      <Container size="sm" sx={{ height: "calc(100vh - 70px)" }}>
+        <Center sx={{ height: "100%" }}>
+          <h1>hello world!</h1>
+        </Center>
+      </Container>
+    </AppShell>
   )
 }
 
---
+++ src/DarkModeToggle.jsx
@@ -0,0 +1,48 @@
+import { createStyles, Switch, useMantineColorScheme } from "@mantine/core"
+import { MoonStars, Sun } from "tabler-icons-react"
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    position: "relative",
+    "& *": {
+      cursor: "pointer",
+    },
+  },
+
+  icon: {
+    pointerEvents: "none",
+    position: "absolute",
+    zIndex: 1,
+    top: 3,
+  },
+
+  iconLight: {
+    left: 4,
+    color: theme.white,
+  },
+
+  iconDark: {
+    right: 4,
+    color: theme.colors.gray[6],
+  },
+}))
+
+const DarkModeToggle = () => {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
+  const { classes, cx } = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <Sun className={cx(classes.icon, classes.iconLight)} size={18} />
+      <MoonStars className={cx(classes.icon, classes.iconDark)} size={18} />
+      <Switch
+        checked={colorScheme === "dark"}
+        onChange={toggleColorScheme}
+        size="md"
+        color="indigo"
+      />
+    </div>
+  )
+}
+
+export default DarkModeToggle

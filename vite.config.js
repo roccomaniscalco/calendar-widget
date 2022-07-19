33e390aa77503ae008582dfe17937c38e5f58be3
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-07-19 05:37:52 -0400

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite"
+import path from "path"
+import react from "@vitejs/plugin-react"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // vite resolves __dirname even though eslint determines it is undefined
+      // eslint-disable-next-line no-undef
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
+})

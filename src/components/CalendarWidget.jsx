b9ec682a2b5415cf78bab02461d20585d0a3d7ac
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-07-19 20:49:48 -0400

    abstract CalendarMonth from CalendarWidget

--- src/components/CalendarWidget.jsx
+++ src/components/CalendarWidget.jsx
@@ -1,42 +1,11 @@
-import { Card, createStyles, Indicator, Text } from "@mantine/core"
-import { Month } from "@mantine/dates"
+import { Card } from "@mantine/core"
 import { useState } from "react"
 import useSWR from "swr"
+import CalendarMonth from "~/components/CalendarMonth"
 import { calendarFormat } from "~/middleware/calendarFormat"
 import { fetchCalendar } from "~/utils/calendar"
 
-const useStyles = createStyles((theme) => ({
-  activeDate: {
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[0]
-        : theme.colors.gray[7],
-    fontSize: theme.fontSizes.xl,
-    marginBottom: theme.spacing.xl,
-    textAlign: "center",
-  },
-
-  // override default styles
-  outside: {
-    opacity: 0,
-  },
-  weekend: {
-    color: `${
-      theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.colors.gray[7]
-    } !important`,
-  },
-  selected: {
-    background: `${
-      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]
-    } !important`,
-    color: `${
-      theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.colors.gray[7]
-    } !important`,
-  },
-}))
-
 const CalendarWidget = () => {
-  const { classes, cx } = useStyles()
   const [activeDate, setActiveDate] = useState(new Date())
   const { data: calendar } = useSWR("/api/calendar", fetchCalendar, {
     use: [calendarFormat],
@@ -49,32 +18,10 @@ const CalendarWidget = () => {
 
   return (
     <Card withBorder p="xl" radius="sm">
-      <Text className={classes.activeDate}>{activeDate.toDateString()}</Text>
-      <Month
-        month={new Date()}
-        value={activeDate}
-        onChange={handleDateChange}
-        disableOutsideEvents
-        dayClassName={(_date, modifiers) =>
-          cx({
-            // order matters here
-            [classes.outside]: modifiers.outside,
-            [classes.weekend]: modifiers.weekend,
-            [classes.selected]: modifiers.selected,
-          })
-        }
-        renderDay={(date) => {
-          return (
-            <Indicator
-              disabled={!calendar.has(date.toISOString())}
-              color="red"
-              size={5}
-              offset={10}
-            >
-              {date.getDate()}
-            </Indicator>
-          )
-        }}
+      <CalendarMonth
+        calendar={calendar}
+        activeDate={activeDate}
+        handleDateChange={handleDateChange}
       />
     </Card>
   )

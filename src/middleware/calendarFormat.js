e42451071682245e763782661323258182582820
Rocco Maniscalco <rmaniscalco@magmutual.com>
2022-08-03 23:12:57 -0400

    add priority for each day in calendarFormat

--- src/middleware/calendarFormat.js
+++ src/middleware/calendarFormat.js
@@ -1,8 +1,21 @@
+import { priority } from "~/constants/priority"
+
+const getHighestPriority = (events) => {
+  const uniquePriorities = new Set(events.map((event) => event.priority))
+  if (uniquePriorities.has(priority.HIGH)) return priority.HIGH
+  if (uniquePriorities.has(priority.MEDIUM)) return priority.MEDIUM
+  if (uniquePriorities.has(priority.LOW)) return priority.LOW
+  return undefined
+}
+
 // convert calendar to Map for perf :P
 function calendarToMap(calendar) {
   const calendarMap = new Map()
   Object.entries(calendar).forEach(([dayIso, day]) => {
-    calendarMap.set(dayIso, day)
+    calendarMap.set(dayIso, {
+      priority: getHighestPriority(day.events),
+      ...day,
+    })
   })
   return calendarMap
 }
